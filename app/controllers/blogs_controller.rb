13d8827638281583f1fb7e6e7914d745f8d52526
noriko ogihara <aloha.nori1104@gmail.com>
2020-08-03 18:03:01 +0900

    新規作成

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,34 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+  def new
+    @blog = Blog.new
+  end
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to new_blog_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
+  end
+  def show
+    @blog = Blog.find(params[:id])
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+end
